e5925c11d2c27e44d445dcb835ad6a0cc036b0fb
jek8484 <ysms3611@gmail.com>
2024-11-19 15:50:52 +0900

    18강

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -7,22 +7,20 @@ public class Main {
         App app = new App();
         app.run();
     }
+
 }
 
 class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        // 생성
         Scanner scanner = new Scanner(System.in);
 
         while ( true ) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
 
-            if ( cmd.equals("종료") ) {
-                break;
-            }
+            if (cmd.equals("종료")) break;
         }
 
         scanner.close();
